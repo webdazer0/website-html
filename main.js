a5984bcaf11b680b083262095624c4f34102aca7
mito343 <webdazer0@gmail.com>
2020-08-10 16:43:59 +0200

    1st

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.card-banner-one', { delay: 500 });
+ScrollReveal().reveal('.card-banner-two', { delay: 500 });
